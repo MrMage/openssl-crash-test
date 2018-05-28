01dea82eb0cce64f7b3d3b1ce98ca72e52522c81
Daniel Alm <Daniel.Alm@ForumD.net>
2018-05-28 11:11:34 +0200

    Add a comment.

--- Sources/openssl-crash-test/main.swift
+++ Sources/openssl-crash-test/main.swift
@@ -6,6 +6,7 @@ do {
     let x = try SSLContext(configuration: TLSConfiguration.forClient())
     print("bar"); fflush(stdout)
 }
+// bar2 will not be printed because the Segmentation Fault occurs before.
 print("bar2"); fflush(stdout)
 
 struct openssl_crash_test {
